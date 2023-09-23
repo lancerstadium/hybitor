fea41738228cc80086708fc4fadba806eb50771e
lancerstadium <lancerstadium@163.com>
2023-09-23 15:27:22 +0800

    lift_v1.4

---
+++ build/hello_x86_64.S
@@ -0,0 +1,503 @@
+Section :.interp
+Section :.note.gnu.property
+0x338: add al, 0
+0x33a: add byte ptr [rax], al
+0x33c: and byte ptr [rax], al
+0x33e: add byte ptr [rax], al
+0x340: add eax, 0x47000000
+0x345: push rbp
+0x347: add byte ptr [rdx], al
+0x349: add byte ptr [rax], al
+0x34b: rol byte ptr [rax + rax], 0
+0x34f: add byte ptr [rbx], al
+0x351: add byte ptr [rax], al
+0x353: add byte ptr [rax], al
+0x355: add byte ptr [rax], al
+0x357: add byte ptr [rdx], al
+0x359: add byte ptr [rax], 0xc0
+0x35c: add al, 0
+0x35e: add byte ptr [rax], al
+0x360: add dword ptr [rax], eax
+0x362: add byte ptr [rax], al
+0x364: add byte ptr [rax], al
+0x366: add byte ptr [rax], al
+Section :.note.gnu.build-id
+0x368: add al, 0
+0x36a: add byte ptr [rax], al
+0x36c: adc al, 0
+0x36e: add byte ptr [rax], al
+0x370: add eax, dword ptr [rax]
+0x372: add byte ptr [rax], al
+0x374: push rbp
+0x377: add byte ptr [rsi + 0x5f], ah
+0x37a: ja 0x3f1
+0x37d: lahf 
+0x37e: push 0x47
+Section :.note.ABI-tag
+0x38c: add al, 0
+0x38e: add byte ptr [rax], al
+0x390: adc byte ptr [rax], al
+0x392: add byte ptr [rax], al
+0x394: add dword ptr [rax], eax
+0x396: add byte ptr [rax], al
+0x398: push rbp
+0x39b: add byte ptr [rax], al
+0x39d: add byte ptr [rax], al
+0x39f: add byte ptr [rbx], al
+0x3a1: add byte ptr [rax], al
+0x3a3: add byte ptr [rdx], al
+0x3a5: add byte ptr [rax], al
+0x3a7: add byte ptr [rax], al
+0x3a9: add byte ptr [rax], al
+Section :.gnu.hash
+0x3b0: add al, byte ptr [rax]
+0x3b2: add byte ptr [rax], al
+Section :.dynsym
+0x3d8: add byte ptr [rax], al
+0x3da: add byte ptr [rax], al
+0x3dc: add byte ptr [rax], al
+0x3de: add byte ptr [rax], al
+0x3e0: add byte ptr [rax], al
+0x3e2: add byte ptr [rax], al
+0x3e4: add byte ptr [rax], al
+0x3e6: add byte ptr [rax], al
+0x3e8: add byte ptr [rax], al
+0x3ea: add byte ptr [rax], al
+0x3ec: add byte ptr [rax], al
+0x3ee: add byte ptr [rax], al
+0x3f0: adc byte ptr [rax], al
+0x3f2: add byte ptr [rax], al
+0x3f4: adc al, byte ptr [rax]
+0x3f6: add byte ptr [rax], al
+0x3f8: add byte ptr [rax], al
+0x3fa: add byte ptr [rax], al
+0x3fc: add byte ptr [rax], al
+0x3fe: add byte ptr [rax], al
+0x400: add byte ptr [rax], al
+0x402: add byte ptr [rax], al
+0x404: add byte ptr [rax], al
+0x406: add byte ptr [rax], al
+0x408: add byte ptr [rax], al
+0x40b: add byte ptr [rax], ah
+0x40d: add byte ptr [rax], al
+0x40f: add byte ptr [rax], al
+0x411: add byte ptr [rax], al
+0x413: add byte ptr [rax], al
+0x415: add byte ptr [rax], al
+0x417: add byte ptr [rax], al
+0x419: add byte ptr [rax], al
+0x41b: add byte ptr [rax], al
+0x41d: add byte ptr [rax], al
+0x41f: add byte ptr [rdx], ah
+0x421: add byte ptr [rax], al
+0x423: add byte ptr [rdx], dl
+0x425: add byte ptr [rax], al
+0x427: add byte ptr [rax], al
+0x429: add byte ptr [rax], al
+0x42b: add byte ptr [rax], al
+0x42d: add byte ptr [rax], al
+0x42f: add byte ptr [rax], al
+0x431: add byte ptr [rax], al
+0x433: add byte ptr [rax], al
+0x435: add byte ptr [rax], al
+0x437: add byte ptr [rax + rax], ah
+0x43b: add byte ptr [rax], ah
+0x43d: add byte ptr [rax], al
+0x43f: add byte ptr [rax], al
+0x441: add byte ptr [rax], al
+0x443: add byte ptr [rax], al
+0x445: add byte ptr [rax], al
+0x447: add byte ptr [rax], al
+0x449: add byte ptr [rax], al
+0x44b: add byte ptr [rax], al
+0x44d: add byte ptr [rax], al
+0x44f: add byte ptr [rbx], dh
+0x452: add byte ptr [rax], al
+0x454: and byte ptr [rax], al
+0x456: add byte ptr [rax], al
+0x458: add byte ptr [rax], al
+0x45a: add byte ptr [rax], al
+0x45c: add byte ptr [rax], al
+0x45e: add byte ptr [rax], al
+0x460: add byte ptr [rax], al
+0x462: add byte ptr [rax], al
+0x464: add byte ptr [rax], al
+0x466: add byte ptr [rax], al
+0x468: add dword ptr [rax], eax
+0x46a: add byte ptr [rax], al
+0x46c: and al, byte ptr [rax]
+0x46e: add byte ptr [rax], al
+0x470: add byte ptr [rax], al
+0x472: add byte ptr [rax], al
+0x474: add byte ptr [rax], al
+0x476: add byte ptr [rax], al
+0x478: add byte ptr [rax], al
+0x47a: add byte ptr [rax], al
+0x47c: add byte ptr [rax], al
+0x47e: add byte ptr [rax], al
+Section :.dynstr
+0x480: add byte ptr [rdi + 0x5f], bl
+Section :.gnu.version
+0x50e: add byte ptr [rax], al
+0x510: add al, byte ptr [rax]
+0x512: add dword ptr [rax], eax
+0x514: add eax, dword ptr [rax]
+0x516: add dword ptr [rax], eax
+0x518: add dword ptr [rax], eax
+0x51a: add eax, dword ptr [rax]
+Section :.gnu.version_r
+0x520: add dword ptr [rax], eax
+0x522: add al, byte ptr [rax]
+Section :.rela.dyn
+0x550: mov eax, 0x3d
+0x555: add byte ptr [rax], al
+0x557: add byte ptr [rax], cl
+0x559: add byte ptr [rax], al
+0x55b: add byte ptr [rax], al
+0x55d: add byte ptr [rax], al
+0x55f: add byte ptr [rax + 0x11], al
+0x562: add byte ptr [rax], al
+0x564: add byte ptr [rax], al
+0x566: add byte ptr [rax], al
+0x568: sar byte ptr [rip], 0
+0x56f: add byte ptr [rax], cl
+0x571: add byte ptr [rax], al
+0x573: add byte ptr [rax], al
+0x575: add byte ptr [rax], al
+0x577: add byte ptr [rax], al
+0x579: adc dword ptr [rax], eax
+0x57b: add byte ptr [rax], al
+0x57d: add byte ptr [rax], al
+0x57f: add byte ptr [rax], cl
+0x581: add byte ptr [rax], al
+0x584: add byte ptr [rax], al
+0x586: add byte ptr [rax], al
+0x588: or byte ptr [rax], al
+0x58a: add byte ptr [rax], al
+0x58c: add byte ptr [rax], al
+0x58e: add byte ptr [rax], al
+0x590: or byte ptr [rax], al
+0x593: add byte ptr [rax], al
+0x595: add byte ptr [rax], al
+0x597: add al, bl
+Section :.rela.plt
+0x610: sar byte ptr [rdi], 1
+0x612: add byte ptr [rax], al
+0x614: add byte ptr [rax], al
+0x616: add byte ptr [rax], al
+Section :.init
+0x1000: endbr64 
+0x1004: sub rsp, 8
+0x1008: mov rax, qword ptr [rip + 0x2fd9]
+0x100f: test rax, rax
+0x1012: je 0x1016
+0x1014: call rax
+0x1016: add rsp, 8
+0x101a: ret 
+Section :.plt
+0x1020: push qword ptr [rip + 0x2f9a]
+0x1026: bnd jmp qword ptr [rip + 0x2f9b]
+0x102d: nop dword ptr [rax]
+0x1030: endbr64 
+0x1034: push 0
+0x1039: bnd jmp 0x1020
+0x103f: nop 
+Section :.plt.got
+0x1040: endbr64 
+0x1044: bnd jmp qword ptr [rip + 0x2fad]
+0x104b: nop dword ptr [rax + rax]
+Section :.plt.sec
+0x1050: endbr64 
+0x1054: bnd jmp qword ptr [rip + 0x2f75]
+0x105b: nop dword ptr [rax + rax]
+Section :.text
+0x1060: endbr64 
+0x1064: xor ebp, ebp
+0x1066: mov r9, rdx
+0x1069: pop rsi
+0x106a: mov rdx, rsp
+0x106d: and rsp, 0xfffffffffffffff0
+0x1071: push rax
+0x1072: push rsp
+0x1073: xor r8d, r8d
+0x1076: xor ecx, ecx
+0x1078: lea rdi, [rip + 0xca]
+0x107f: call qword ptr [rip + 0x2f53]
+0x1085: hlt 
+0x1086: nop word ptr cs:[rax + rax]
+0x1090: lea rdi, [rip + 0x2f79]
+0x1097: lea rax, [rip + 0x2f72]
+0x109e: cmp rax, rdi
+0x10a1: je 0x10b8
+0x10a3: mov rax, qword ptr [rip + 0x2f36]
+0x10aa: test rax, rax
+0x10ad: je 0x10b8
+0x10af: jmp rax
+0x10b1: nop dword ptr [rax]
+0x10b8: ret 
+0x10b9: nop dword ptr [rax]
+0x10c0: lea rdi, [rip + 0x2f49]
+0x10c7: lea rsi, [rip + 0x2f42]
+0x10ce: sub rsi, rdi
+0x10d1: mov rax, rsi
+0x10d4: shr rsi, 0x3f
+0x10d8: sar rax, 3
+0x10dc: add rsi, rax
+0x10df: sar rsi, 1
+0x10e2: je 0x10f8
+0x10e4: mov rax, qword ptr [rip + 0x2f05]
+0x10eb: test rax, rax
+0x10ee: je 0x10f8
+0x10f0: jmp rax
+0x10f2: nop word ptr [rax + rax]
+0x10f8: ret 
+0x10f9: nop dword ptr [rax]
+0x1100: endbr64 
+0x1104: cmp byte ptr [rip + 0x2f05], 0
+0x110b: jne 0x1138
+0x110d: push rbp
+0x110e: cmp qword ptr [rip + 0x2ee2], 0
+0x1116: mov rbp, rsp
+0x1119: je 0x1127
+0x111b: mov rdi, qword ptr [rip + 0x2ee6]
+0x1122: call 0x1040
+0x1127: call 0x1090
+0x112c: mov byte ptr [rip + 0x2edd], 1
+0x1133: pop rbp
+0x1134: ret 
+0x1135: nop dword ptr [rax]
+0x1138: ret 
+0x1139: nop dword ptr [rax]
+0x1140: endbr64 
+0x1144: jmp 0x10c0
+0x1149: endbr64 
+0x114d: push rbp
+0x114e: mov rbp, rsp
+0x1151: lea rax, [rip + 0xeac]
+0x1158: mov rdi, rax
+0x115b: call 0x1050
+0x1160: mov eax, 0
+0x1165: pop rbp
+0x1166: ret 
+Section :.fini
+0x1168: endbr64 
+0x116c: sub rsp, 8
+0x1170: add rsp, 8
+0x1174: ret 
+Section :.rodata
+0x2000: add dword ptr [rax], eax
+0x2002: add al, byte ptr [rax]
+0x2004: push 0x6f6c6c65
+0x2009: sub al, 0x20
+0x200b: ja 0x207c
+0x200d: jb 0x207b
+0x200f: and dword ptr fs:[rax], eax
+Section :.eh_frame_hdr
+0x2014: add dword ptr [rbx], ebx
+0x2016: add edi, dword ptr [rbx]
+0x2018: xor byte ptr [rax], al
+0x201a: add byte ptr [rax], al
+0x201c: add eax, 0xc000000
+Section :.eh_frame
+0x2048: adc al, 0
+0x204a: add byte ptr [rax], al
+0x204c: add byte ptr [rax], al
+0x204e: add byte ptr [rax], al
+0x2050: add dword ptr [rdx + 0x52], edi
+0x2053: add byte ptr [rcx], al
+0x2055: js 0x2067
+0x2057: add dword ptr [rbx], ebx
+0x2059: or al, 7
+0x205b: or byte ptr [rax + 0x14000001], dl
+0x2061: add byte ptr [rax], al
+0x2063: add byte ptr [rax + rax], bl
+0x2066: add byte ptr [rax], al
+0x2068: clc 
+0x2069: out dx, eax
+Section :.init_array
+0x3db8: adc dword ptr [rax], eax
+0x3dbb: add byte ptr [rax], al
+0x3dbd: add byte ptr [rax], al
+Section :.fini_array
+0x3dc0: add byte ptr [rcx], dl
+0x3dc2: add byte ptr [rax], al
+0x3dc4: add byte ptr [rax], al
+0x3dc6: add byte ptr [rax], al
+Section :.dynamic
+0x3dc8: add dword ptr [rax], eax
+0x3dca: add byte ptr [rax], al
+0x3dcc: add byte ptr [rax], al
+0x3dce: add byte ptr [rax], al
+Section :.got
+0x3fb8: enter 0x3d, 0
+0x3fbc: add byte ptr [rax], al
+0x3fbe: add byte ptr [rax], al
+0x3fc0: add byte ptr [rax], al
+0x3fc2: add byte ptr [rax], al
+0x3fc4: add byte ptr [rax], al
+0x3fc6: add byte ptr [rax], al
+0x3fc8: add byte ptr [rax], al
+0x3fca: add byte ptr [rax], al
+0x3fcc: add byte ptr [rax], al
+0x3fce: add byte ptr [rax], al
+0x3fd0: xor byte ptr [rax], dl
+0x3fd2: add byte ptr [rax], al
+0x3fd4: add byte ptr [rax], al
+0x3fd6: add byte ptr [rax], al
+0x3fd8: add byte ptr [rax], al
+0x3fda: add byte ptr [rax], al
+0x3fdc: add byte ptr [rax], al
+0x3fde: add byte ptr [rax], al
+0x3fe0: add byte ptr [rax], al
+0x3fe2: add byte ptr [rax], al
+0x3fe4: add byte ptr [rax], al
+0x3fe6: add byte ptr [rax], al
+0x3fe8: add byte ptr [rax], al
+0x3fea: add byte ptr [rax], al
+0x3fec: add byte ptr [rax], al
+0x3fee: add byte ptr [rax], al
+0x3ff0: add byte ptr [rax], al
+0x3ff2: add byte ptr [rax], al
+0x3ff4: add byte ptr [rax], al
+0x3ff6: add byte ptr [rax], al
+0x3ff8: add byte ptr [rax], al
+0x3ffa: add byte ptr [rax], al
+0x3ffc: add byte ptr [rax], al
+0x3ffe: add byte ptr [rax], al
+Section :.data
+0x4000: add byte ptr [rax], al
+0x4002: add byte ptr [rax], al
+0x4004: add byte ptr [rax], al
+0x4006: add byte ptr [rax], al
+0x4008: or byte ptr [rax], al
+0x400b: add byte ptr [rax], al
+0x400d: add byte ptr [rax], al
+Section :.bss
+0x4010: cmp spl, byte ptr [r8]
+0x4015: sub byte ptr [rbp + 0x62], dl
+Section :.comment
+0x0: cmp spl, byte ptr [r8]
+0x5: sub byte ptr [rbp + 0x62], dl
+0x8: jne 0x78
+0xa: je 0x81
+0xc: and byte ptr [rcx], dh
+0xe: xor dword ptr [rsi], ebp
+0x10: xor al, 0x2e
+0x12: xor byte ptr [rip + 0x75627531], ch
+0x18: outsb dx, byte ptr [rsi]
+0x19: je 0x90
+0x1b: xor dword ptr [rsi + 0x32], edi
+0x1e: xor ch, byte ptr [rsi]
+0x20: xor byte ptr [rcx + rbp], dh
+0x23: and byte ptr [rcx], dh
+0x25: xor dword ptr [rsi], ebp
+0x27: xor al, 0x2e
+0x29: xor byte ptr [rax], al
+Section :.symtab
+0x0: add byte ptr [rax], al
+0x2: add byte ptr [rax], al
+0x4: add byte ptr [rax], al
+0x6: add byte ptr [rax], al
+0x8: add byte ptr [rax], al
+0xa: add byte ptr [rax], al
+0xc: add byte ptr [rax], al
+0xe: add byte ptr [rax], al
+0x10: add byte ptr [rax], al
+0x12: add byte ptr [rax], al
+0x14: add byte ptr [rax], al
+0x16: add byte ptr [rax], al
+0x18: add dword ptr [rax], eax
+0x1a: add byte ptr [rax], al
+0x1c: add al, 0
+0x1e: int1 
+0x1f: inc dword ptr [rax]
+0x21: add byte ptr [rax], al
+0x23: add byte ptr [rax], al
+0x25: add byte ptr [rax], al
+0x27: add byte ptr [rax], al
+0x29: add byte ptr [rax], al
+0x2b: add byte ptr [rax], al
+0x2d: add byte ptr [rax], al
+0x2f: add byte ptr [rcx], cl
+0x31: add byte ptr [rax], al
+0x33: add byte ptr [rcx], al
+0x35: add byte ptr [rax + rax], al
+0x38: mov word ptr [rbx], es
+0x3a: add byte ptr [rax], al
+0x3c: add byte ptr [rax], al
+0x3e: add byte ptr [rax], al
+0x40: and byte ptr [rax], al
+0x42: add byte ptr [rax], al
+0x44: add byte ptr [rax], al
+0x46: add byte ptr [rax], al
+0x48: adc eax, dword ptr [rax]
+0x4a: add byte ptr [rax], al
+0x4c: add al, 0
+0x4e: int1 
+0x4f: inc dword ptr [rax]
+0x51: add byte ptr [rax], al
+0x53: add byte ptr [rax], al
+0x55: add byte ptr [rax], al
+0x57: add byte ptr [rax], al
+0x59: add byte ptr [rax], al
+0x5b: add byte ptr [rax], al
+0x5d: add byte ptr [rax], al
+0x5f: add byte ptr [rsi], bl
+0x61: add byte ptr [rax], al
+0x63: add byte ptr [rdx], al
+0x65: add byte ptr [rax], dl
+0x67: add byte ptr [rax + 0x10], dl
+0x6d: add byte ptr [rax], al
+0x6f: add byte ptr [rax], al
+0x71: add byte ptr [rax], al
+0x73: add byte ptr [rax], al
+0x75: add byte ptr [rax], al
+0x77: add byte ptr [rax], ah
+0x79: add byte ptr [rax], al
+0x7b: add byte ptr [rdx], al
+0x7d: add byte ptr [rax], dl
+0x7f: add al, al
+0x81: adc byte ptr [rax], al
+0x83: add byte ptr [rax], al
+0x85: add byte ptr [rax], al
+0x87: add byte ptr [rax], al
+0x89: add byte ptr [rax], al
+0x8b: add byte ptr [rax], al
+0x8d: add byte ptr [rax], al
+0x8f: add byte ptr [rbx], dh
+0x91: add byte ptr [rax], al
+0x93: add byte ptr [rdx], al
+0x95: add byte ptr [rax], dl
+0x97: add byte ptr [rax], al
+0x99: adc dword ptr [rax], eax
+0x9b: add byte ptr [rax], al
+0x9d: add byte ptr [rax], al
+0x9f: add byte ptr [rax], al
+0xa1: add byte ptr [rax], al
+0xa3: add byte ptr [rax], al
+0xa5: add byte ptr [rax], al
+0xa7: add byte ptr [rcx], cl
+0xaa: add byte ptr [rax], al
+0xac: add dword ptr [rax], eax
+0xae: sbb al, byte ptr [rax]
+0xb0: adc byte ptr [rax], al
+0xb3: add byte ptr [rax], al
+0xb5: add byte ptr [rax], al
+0xb7: add byte ptr [rcx], al
+0xb9: add byte ptr [rax], al
+0xbb: add byte ptr [rax], al
+0xbd: add byte ptr [rax], al
+0xbf: add byte ptr [rbp], dl
+0xc2: add byte ptr [rax], al
+0xc4: add dword ptr [rax], eax
+Section :.strtab
+0x0: add byte ptr [rbx + 0x63], dl
+0x3: jb 0x79
+0x5: xor dword ptr [rsi], ebp
+0x7: outsd dx, dword ptr [rsi]
+0x8: add byte ptr [rdi + 0x5f], bl
+Section :.shstrtab
+0x0: add byte ptr [rsi], ch
+0x2: jae 0x7d
+0x4: insd dword ptr [rdi], dx
+0x5: je 0x68
