48e55db949a22b666c7ed8a7c480c92bc5eeb3a8
lancerstadium <lancerstadium@163.com>
2023-10-09 21:32:50 +0800

    effect hybitor iterm build

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 /// \file main.cpp
-/// \brief 接收并解析命令行参数，加载合适的 libtinycode-*.so
+/// \brief 接收并解析命令行参数
 
 // 标准库
 #include <iostream>
 
 
 // 本地库
-#include "cliparser/cliparser.hpp"
 
 // 引用
 using std::cout;
@@ -18,10 +17,7 @@ using std::endl;
 int main(int argc, char **argv)
 {
     // --- 解析执行输入参数 ---
-    cliparser cp;                      
-    cp.cli_init();              // 初始化 CLI 环境
-    cp.cli_parse(argc, argv);   // 解析参数
-    cp.cli_exec();    // CLI 执行命令
+    cout << "hello, world\n" << endl;
 
 	return 0;
 }
